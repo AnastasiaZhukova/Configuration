d6f6b6406cf201cacfef28d8bffc472b817488c5
AnastasiaZhukova <azhuk.mail@gmail.com>
2017-10-25 15:45:15 +0300

    Added separate backends

--- app/src/main/java/com/github/anastasiazhukova/configuration/Constants.java
+++ app/src/main/java/com/github/anastasiazhukova/configuration/Constants.java
@@ -2,6 +2,6 @@ package com.github.anastasiazhukova.configuration;
 
 public final class Constants {
 
-    public static final String CALCULATOR_URL = "https://add-calculator.appspot.com/";
+    public static final String CALCULATOR_URL = BuildConfig.CALCULATOR_URL;
 
 }
